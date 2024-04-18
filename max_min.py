c607fd042b616cdf05425946f2f5590f46314818
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-04-18 19:01:42 +0530

    2nd

---
+++ max_min.py
@@ -0,0 +1,12 @@
+def maximum_min(array):
+    maximum_value = max(array)
+    minimum_value = min(array)
+
+    print("maximum value of this array is: ",maximum_value)
+    print("minimum value of this array is: ",minimum_value)
+
+
+
+array = [1,2,3,4,5]
+
+maximum_min(array)
--- palindrome.py
+++ palindrome.py
@@ -5,3 +5,5 @@ my_str = "english"
 result = palindrome(my_str)
 
 print(f"the given string {my_str} is a palindrome {result}")
+
+
